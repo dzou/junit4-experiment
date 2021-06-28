807f7775e46361a279161ee8ed0eef85352999f5
dzou <dzou@users.noreply.github.com>
2021-06-28 17:47:14 -0400

    add parent pom

--- child/src/test/java/example/OtherTest.java
+++ child/src/test/java/example/OtherTest.java
@@ -1,8 +1,9 @@
-package com.example;
+package example;
 
-import static org.junit.Assert.assertTrue;
 
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
 
 public class OtherTest {
 
---
+++ src/test/java/com/FooTest.java
@@ -0,0 +1,12 @@
+package com;
+
+import org.junit.Test;
+
+public class FooTest {
+
+  @Test
+  public void test() {
+    System.out.println("Hello");
+  }
+
+}
